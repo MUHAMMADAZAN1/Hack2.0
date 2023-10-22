69dfe51ab41763733c00ae79a0c0b3dd2b711586
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-10-22 19:33:01 +0500

    variables dicleared

---
+++ airDistance.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//include math library to use it function
+#include<math.h>
+//the program about to find distance between two place
+int main(){
+    //made variable according the program
+    double latitudeOrigin,latitudeDestination,longtitudeOrigin,longtitudeDestination;
+    double airDistance, delta
+    return 0;
+}
