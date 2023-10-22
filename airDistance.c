7b7b2fe32601ef756b4dcb62dfd0e2104bad8768
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-10-22 19:39:00 +0500

    ask from user to provide value

--- airDistance.c
+++ airDistance.c
@@ -6,6 +6,13 @@ int main(){
     //made variable according the program
     double latitudo,latitudeDesti,longtitudeO,longtitudeDesti;
     double airDistance, delta
-
+ //made constant of rediuos of the earth 
+    const float rediousEarth= 6371;
+    //prompt msg to user to get input and user should give input in degree
+    printf("\t\tEnter the values in Degree\t\t\n");
+    printf("Enter the value of Latitude and Longtitude of Origin(Latitude,longtitude):");
+    scanf("%lf,%lf",&latitudeO,&longtitudeO);
+    printf("Enter the value of Latitude and Longtitude of Destination(latitude ,longtitude):");
+    scanf("%lf,%lf",&latitudeDesti,&longtitudeDesti);
     return 0;
 }
